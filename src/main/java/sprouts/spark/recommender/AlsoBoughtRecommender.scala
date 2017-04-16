a812b5bfbca2a9289f8c1c0b6e2a067ee0825c7c
AlbertoAreLun <alberto-arelu@hotmail.com>
2017-04-15 17:07:45 -0700

    Refactor: update job alsoBoughtRecommender

--- src/main/java/sprouts/spark/recommender/AlsoBoughtRecommender.scala
+++ src/main/java/sprouts/spark/recommender/AlsoBoughtRecommender.scala
@@ -26,7 +26,7 @@ import sprouts.spark.utils.WriteMongoDB
 import sprouts.spark.utils.ReadMySQL
 import org.apache.spark.sql.functions._
 
-case class AlsoBoughtRecommender(int1:Int,int2:Int,int3:Long)
+case class AlsoBoughtRecommender(itemID: Int, alsoBought: List[(Int, Long)])
 
 object AlsoBoughtRecommender extends SparkJob {
   override def runJob(sc: SparkContext, jobConfig: Config): Any = {
@@ -41,43 +41,31 @@ object AlsoBoughtRecommender extends SparkJob {
     val sqlContext = SQLContext.getOrCreate(sc)
 
     // Query to MySQL
-    val df = ReadMySQL.read("""(SELECT ordereditem.item_id , ordereditem.order_id 
+    val df = ReadMySQL.read("""(SELECT ordereditem.item_id , ordereditem.order_id ,ordereditem.quantity
         FROM `digital-music`.ordereditem 
-        where ordereditem.item_id=116720 or ordereditem.item_id=116534
-        
        ) AS data""", sqlContext)
-      
-    val df2 = ReadMySQL.read("""(SELECT ordereditem.item_id as r_item_id, ordereditem.order_id as r_order_id, ordereditem.quantity
-        FROM `digital-music`.ordereditem 
-        where ordereditem.item_id=116534 or ordereditem.item_id=116720
-       ) AS data""", sqlContext)
-    val results = df.join(df2,df.col("order_id") === df2.col("r_order_id") and df.col("item_id").notEqual(df2.col("r_item_id")))
-    .groupBy(
-  df.col("item_id"), df2.col("r_item_id")
-).agg(
-  df.col("item_id"),df2.col("r_item_id"), sum(df2.col("quantity"))
-)
-      
-      print("Primera fase")
-    val pri=results.select(results.col("item_id"),results.col("r_item_id"),results.col("sum(quantity)"))
-    .map{  x => (x.getInt(0),x.getInt(1),x.getLong(2))}
-    
-    
-    
-    
-     // DF to save in MongoDB
+
+    val results = df.join(df.select(col("item_id").alias("r_item_id"), col("order_id").alias("r_order_id"), col("quantity").alias("weight")), col("order_id") === col("r_order_id") and col("item_id").notEqual(col("r_item_id")))
+      .groupBy(
+        col("item_id"), col("r_item_id")).agg(
+          col("item_id"), col("r_item_id"), sum(col("weight")))
+
+    print("Primera fase")
+    val pri = results.select(results.col("item_id"), results.col("r_item_id"), results.col("sum(weight)"))
+      .map { x => (x.getInt(0), (x.getInt(1), x.getLong(2))) }
+      .aggregateByKey(List[(Int, Long)]())(_ ++ List(_), _ ++ _)
+
+    // DF to save in MongoDB
     val test =
       sqlContext.createDataFrame(
         pri.map {
           x =>
-            AlsoBoughtRecommender(x._1,x._2,x._3) // Map each ellement in RDD with an ItemProfile
+            AlsoBoughtRecommender(x._1, x._2)
         })
 
     // We finally persist the DF into MongoDB to extract it from the dashboard
-    WriteMongoDB.deleteAndPersistDF(test, sqlContext, "newtest")
+    WriteMongoDB.deleteAndPersistDF(test, sqlContext, "also_bought_recommender")
     test.collect()
-    
-      
 
   }
 }
