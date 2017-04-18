7f696f04696c29955e6f97fd4c82eb929a707474
Álvaro Valencia <avalenciaparra@gmail.com>
2017-04-18 12:33:14 -0700

    refactor: added state name abbreviations to FinancesOverview and StockOverview

--- src/main/java/sprouts/spark/finances/FinancesOverview.scala
+++ src/main/java/sprouts/spark/finances/FinancesOverview.scala
@@ -9,9 +9,10 @@ import spark.jobserver.SparkJobValid
 import org.apache.spark.sql.SQLContext
 import sprouts.spark.utils.ReadMySQL
 import sprouts.spark.utils.WriteMongoDB
+import sprouts.spark.utils.ReadMongoDB
 
 case class SaleValue(month: Int, year: Int, SalesValue: Double)
-case class SaleValueByState(month: Int, year: Int, salesValue: Double, state: String)
+case class SaleValueByState(month: Int, year: Int, salesValue: Double, state: String, abbreviation:String)
 case class FinacesOverview(monthly_sales: Array[SaleValue], monthly_sales_by_state: Array[SaleValueByState])
 
 object FinancesOverview extends SparkJob {
@@ -26,8 +27,13 @@ object FinancesOverview extends SparkJob {
   def execute(sc: SparkContext): Any = {
     val sqlContext = SQLContext.getOrCreate(sc)
 
-    val orders = ReadMySQL.read("(SELECT totalPrice, YEAR(date) as year, MONTH(date) as month,state FROM order_ join customer on customer.id = order_.customer_id WHERE date > DATE_SUB(DATE_FORMAT(NOW() ,'%Y-%m-01'), INTERVAL 24 MONTH)) AS data", sqlContext)
-
+    val orders = ReadMySQL.read("(SELECT totalPrice, YEAR(date) as year, MONTH(date) as month,state FROM order_ join customer on customer.id = order_.customer_id WHERE date > DATE_SUB(DATE_FORMAT(NOW() ,'%Y-%m-%d'), INTERVAL 24 MONTH)) AS data", sqlContext)
+    val mapStatesNames = sc.broadcast(ReadMongoDB.read(sqlContext, "map_state_name_abbreviation")
+      .select("name", "abbreviation")
+      .map { x => (x.getString(0), x.getString(1)) } // maps (state_name, abbreviation)
+      .collectAsMap().toMap // convert it to a map
+    )
+    
     // The total monthly sales value during the last 24 months
     val salesValues = orders.map { x => ((x.getLong(1), x.getLong(2)), x.getDouble(0)) }
       .reduceByKey(_ + _)
@@ -39,12 +45,12 @@ object FinancesOverview extends SparkJob {
     val salesValuesByState = orders.map { x => ((x.getLong(1), x.getLong(2), x.getString(3)), x.getDouble(0)) }
       .reduceByKey(_ + _)
       .map {
-        x => SaleValueByState(x._1._2.intValue(), x._1._1.intValue(), x._2, x._1._3);
+        x => SaleValueByState(x._1._2.intValue(), x._1._1.intValue(), x._2, x._1._3, "US-"+mapStatesNames.value.get(x._1._3).get);
       }.collect()
 
     // Save and response
     val finaceOverview =
       sqlContext.createDataFrame(List(FinacesOverview(salesValues, salesValuesByState)))
-    WriteMongoDB.deleteAndPersistDF(finaceOverview, sqlContext, "finance_overview");
+    WriteMongoDB.deleteAndPersistDF(finaceOverview, sqlContext, "finances_overview");
   }
 }
